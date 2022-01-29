b5a774a0eb9790e89d54d653655a34c79c6a7ad4
Ismael Teixeira <ismaelteixeira6@gmail.com>
2022-01-29 10:11:44 -0400

    fixing _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,31 @@
-import Document, { Head } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-M1430073BZ"
-        />
+      <Html>
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-M1430073BZ"
+          />
 
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', 'G-M1430073BZ');          
             `,
-          }}
-        />
-      </Head>
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
     );
   }
 }
